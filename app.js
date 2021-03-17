32916beb712085fd8b28123d28369165bb0a9793
shakilmahammud <hshakil987@gmail>
2021-03-18 00:36:39 +0600

    node

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const http= require('http')
+const http= require('http');
+const fs= require('fs');
 
 function reListener(req,res){
 
 }
 const server=http.createServer((req,res)=>{
-    console.log(req)
+    const url =req.url;
+    if(url==='/'){
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text"/> <button type="submit">Send</button></form></body>');
+    res.write('</htnl>');
+     return res.end()
+    }
 
+    if(url==='/message'){
+         fs.writeFileSync('message.txt','Dummy')
+         res.statusCode=302;
+         res.setHeader('Location','/')
+         return res.end()
+    }
+    console.log(req)
+    // process.exit()
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>');
+    res.write('<head><title>My First Page </title></head>');
+    res.write('<body><h1>Hello Ok server</h1></body>');
+    res.write('</htnl>');
+    res.end()
 })
 
 server.listen(30001)
