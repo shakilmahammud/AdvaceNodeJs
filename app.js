cc848f6e5ede5eede6ee7eb5c1c1b7883dd14341
shakilmahammud <hshakil987@gmail>
2021-03-17 19:50:32 +0600

    create server in node

---
+++ app.js
@@ -0,0 +1,11 @@
+const http= require('http')
+
+function reListener(req,res){
+
+}
+const server=http.createServer((req,res)=>{
+    console.log(req)
+
+})
+
+server.listen(30001)
--- first-app.js
+++ first-app.js
@@ -3,3 +3,25 @@ console.log('Hello from Node.js');
 const fs=require('fs');
 
 fs.writeFileSync('hello.txt','helo form node js')
+
+
+const fetchData=()=>{
+    const promise=new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+            resolve('done')
+        },1500)
+    })
+    return promise;
+}
+
+setTimeout(()=>{
+    console.log('Hello from Node.js');
+    fetchData()
+    .then(txt=>{
+        console.log(txt);
+        return fetchData();
+    })
+    .then(text2=>{
+        console.log(text2);
+    })
+   },2000)
